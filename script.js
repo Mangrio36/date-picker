18993d0d05c878cf66004a115da783cd0c05a731
Kamran Ali <kamranleembo12@gmail.com>
2022-11-28 19:34:25 +0500

    prev and next month

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+import { format } from "date-fns";
 const datePickerBtn = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
+const preMonth = document.querySelector(".prev-month-button");
+const nextMonth = document.querySelector(".next-month-button");
 
 datePickerBtn.addEventListener("click", () => {
   datePicker.classList.toggle("show");
 });
+
+function setDate(date) {
+  datePickerBtn.innerText = format(date, "MMMM do, yyyy");
+}
+setDate(new Date());
