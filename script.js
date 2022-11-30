cc9e32c4faac347163fa1d9df68a4b0d9d22fa4c
Kamran Ali <kamranleembo12@gmail.com>
2022-11-30 07:00:35 +0500

    changed

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-import { format } from "date-fns";
+import { format, getUnixTime, fromUnixTime } from "date-fns";
 const datePickerBtn = document.querySelector(".date-picker-button");
 const datePicker = document.querySelector(".date-picker");
+const datePickerHeaderText = document.querySelector(".current-month");
 const preMonth = document.querySelector(".prev-month-button");
 const nextMonth = document.querySelector(".next-month-button");
 
 datePickerBtn.addEventListener("click", () => {
   datePicker.classList.toggle("show");
+  const selectedDate = fromUnixTime(datePickerBtn.dataset.selectedDate);
+  setupDatePicker(selectedDate);
 });
 
+function setupDatePicker(selectedDate) {
+  datePickerHeaderText.innerText = format(selectedDate, "MMMM-yyyy");
+}
 function setDate(date) {
   datePickerBtn.innerText = format(date, "MMMM do, yyyy");
+  datePickerBtn.dataset.selectedDate = getUnixTime(date);
 }
 setDate(new Date());
